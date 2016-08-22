a07ce004a69d730851d5bffa275291e1ed37df6f
Jesse Jaggars <jjaggars@redhat.com>
2016-08-22 15:06:57 -0500

    Adding cib shared mapper

---
+++ falafel/mappers/cib.py
@@ -0,0 +1,25 @@
+from falafel.core.plugins import mapper
+from falafel.core import MapperOutput, computed
+
+import xml.etree.ElementTree as ET
+
+
+@mapper("cib.xml")
+class CIB(MapperOutput):
+    """
+    Wraps a DOM of cib.xml
+
+    self.dom is an instance of ElementTree.
+    """
+
+    def __init__(self, data, path=None):
+        self.dom = ET.fromstring(data)
+        super(CIB, self).__init__(data, path)
+
+    @staticmethod
+    def parse_content(content):
+        return "\n".join(content)
+
+    @computed
+    def nodes(self):
+        return [n.get("uname").lower() for n in self.dom.findall(".//nodes/node")]
