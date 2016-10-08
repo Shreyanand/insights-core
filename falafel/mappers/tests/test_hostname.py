73e26a9edc4a364659c3df3267fc341e7794ec2b
Xiangce Liu <xiangceliu@redhat.com>
2016-10-08 13:27:50 +0800

    Update hostname to support foreman-debug archive's `facts`
    - fix flake8 errors in df

--- falafel/mappers/tests/test_hostname.py
+++ falafel/mappers/tests/test_hostname.py
@@ -2,6 +2,24 @@ from falafel.mappers import hostname
 from falafel.tests import context_wrap
 
 HOSTNAME = "rhel7.example.com"
+HOSTNAME_SHORT = "rhel7"
+FACTS_FQDN = """
+COMMAND> facter
+
+architecture => x86_64
+augeasversion => 1.1.0
+facterversion => 1.7.6
+filesystems => xfs
+fqdn => ewa-satellite.cs.boeing.com
+domain => cs.boeing.com
+""".strip()
+FACTS_NO_FQDN = """
+COMMAND> facter
+
+architecture => x86_64
+facterversion => 1.7.6
+filesystems => xfs
+""".strip()
 
 
 class TestHostname():
@@ -10,3 +28,23 @@ class TestHostname():
         assert data.fqdn == "rhel7.example.com"
         assert data.hostname == "rhel7"
         assert data.domain == "example.com"
+
+        data = hostname.Hostname(context_wrap(HOSTNAME_SHORT))
+        assert data.fqdn == "rhel7"
+        assert data.hostname == "rhel7"
+        assert data.domain == ""
+
+        data = hostname.Hostname(context_wrap(FACTS_FQDN))
+        assert data.fqdn == "ewa-satellite.cs.boeing.com"
+        assert data.hostname == "ewa-satellite"
+        assert data.domain == "cs.boeing.com"
+
+        data = hostname.Hostname(context_wrap(FACTS_NO_FQDN))
+        assert data.fqdn is None
+        assert data.hostname is None
+        assert data.domain is None
+
+        data = hostname.Hostname(context_wrap(""))
+        assert data.fqdn is None
+        assert data.hostname is None
+        assert data.domain is None
