142393ae9b3008fbe42f2850a976e5103d34982b
Xiangce Liu <xiangceliu@redhat.com>
2017-06-09 04:18:44 -0500

    Rename 1.x (#72)
    * Rename to insights core and use new terminology
    falafel -> insights
    mapper -> parser
    reducer -> combiner/rule
    
    * Finish the rest part of the Renaming (#71)
    - Revert "/dev/parser" to "/dev/mapper" in comments and test cases
    
    * Rename 1.x (#73)
    * Rename the mappers and reducers in `docs`
    - fix flake8 errors
    - rename `insights/core/reducer.py` to `insights/core/combiner.py`
    * Rename `falafel` to `insights-core`
    - Rebase the `docs` to the original `1.x` branch
    - Rename `falafel` in scripts
    * Rename falafel in .coveragerc
    * Trigger shippable again to check if shippable.yml is updated

---
+++ insights/parsers/foreman_log.py
@@ -0,0 +1,37 @@
+"""
+foreman_log - Files /var/log/foreman...
+=======================================
+
+
+Module for parsing the log files in foreman-debug archive
+
+Note:
+    Please refer to its super-class ``LogFileOutput``
+
+"""
+
+from .. import LogFileOutput, parser
+
+
+@parser('foreman_proxy.log')
+class ProxyLog(LogFileOutput):
+    """Class for parsing ``foreman-proxy/proxy.log`` file."""
+    pass
+
+
+@parser('foreman_satellite.log')
+class SatelliteLog(LogFileOutput):
+    """Class for parsing ``foreman-installer/satellite.log`` file."""
+    pass
+
+
+@parser('foreman_production.log')
+class ProductionLog(LogFileOutput):
+    """Class for parsing ``foreman/production.log`` file."""
+    pass
+
+
+@parser('candlepin.log')
+class CandlepinLog(LogFileOutput):
+    """Class for parsing ``candlepin/candlepin.log`` file."""
+    pass
