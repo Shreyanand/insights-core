5e0654805271b3930f9db6dbcbb8672f9d551743
Paul Wayper <paulway@mabula.net>
2017-08-18 15:04:58 +1000

    RHNConf documentation for master branch (#351)
    * Documentation markup include file from template
    
    * Adding RHNConf documentation and test coverage

--- insights/parsers/rhn_conf.py
+++ insights/parsers/rhn_conf.py
@@ -1,20 +1,32 @@
-from .. import Parser, parser, get_active_lines, LegacyItemAccess
+"""
+RHNConf - file ``/etc/rhn/rhn.conf``
+====================================
+
+"""
+
+from insights import Parser, parser, LegacyItemAccess
+from insights.parsers import get_active_lines
 
 
 @parser('rhn.conf')
 class RHNConf(LegacyItemAccess, Parser):
-    """Class to parse the configuration file ``rhn.conf``
-    Attributes:
-        data (dict): A dict where
-        - keys are the row header
-        - values are the option after the "=".
-          Note: For settings with multiple options, the value is a list.
-
     """
+    Class to parse the configuration file ``rhn.conf``.
 
-    def parse_content(self, content):
-        """
-        ---Sample---
+    The special feature of ``rhn.conf`` is that values can span multiple
+    lines with each intermediate line ending with a comma.
+
+    This parser uses the :class:`insights.core.LegacyItemAccess` mix-in to provide
+    access to its data directly.
+
+    Attributes:
+        data (dict): A dictionary of values keyed by the configuration
+            item.  Values spanning multiple lines are compacted together.
+            Values that include a comma are turned into lists.
+
+
+    Sample ``rhn.conf`` input::
+
         # Corporate gateway (hostname:PORT):
         server.satellite.http_proxy = corporate_gateway.example.com:8080
         server.satellite.http_proxy_username =
@@ -28,9 +40,20 @@ class RHNConf(LegacyItemAccess, Parser):
                                        RHN::Task::SummaryPopulation,
                                        RHN::Task::RHNProc,
                                        RHN::Task::PackageCleanup
-        -----------
-        """
 
+    Examples:
+        >>> conf = shared[RHNConf]
+        >>> conf.data['server.satellite.http_proxy']  # Long form access
+        'corporate_gateway.example.com:8080'
+        >>> conf['server.satellite.http_proxy']  # Short form access
+        'corporate_gateway.example.com:8080'
+        >>> conf['traceback_mail']  # split into a list
+        ['test@pobox.com', 'test@redhat.com']
+        >>> conf['web.default_taskmaster_tasks'][3]  # Values can span multiple lines
+        'RHN::Task::DailySummary'
+    """
+
+    def parse_content(self, content):
         rhn = {}
         is_multi = False
         multi_lines_key = None
