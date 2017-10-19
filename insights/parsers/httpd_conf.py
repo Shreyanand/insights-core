c806e4d284e240b2f57e68718d9cf0812042e7f7
Xiangce Liu <xiangceliu@redhat.com>
2017-10-19 09:05:37 -0500

    Revert #615 (#623)
    Merge `httpd.conf` and `httpd.conf.d` to `httpd_conf` 3.x

--- insights/parsers/httpd_conf.py
+++ insights/parsers/httpd_conf.py
@@ -77,20 +77,13 @@ from collections import namedtuple
 
 import re
 from .. import Parser, parser, get_active_lines, LegacyItemAccess
-from .. import add_filter
 from insights.specs import httpd_conf
-from insights.specs import httpd_conf_d
 
 ParsedData = namedtuple('ParsedData', ['value', 'line', 'section', 'section_name', 'file_name', 'file_path'])
 """namedtuple: Type for storing the parsed httpd configuration's directive information."""
 
 
-add_filter('httpd.conf', ['IncludeOptional'])
-
-
-# TODO: fix stacked decorator
 @parser(httpd_conf)
-@parser(httpd_conf_d)
 class HttpdConf(LegacyItemAccess, Parser):
     """
     Get the key value pairs separated on the first space, ignoring leading
